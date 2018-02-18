b06408906d47dfb7723270036ee46347a02b54dd
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-02-18 20:10:46 +0100

    Fixed IT's related to JDK 9  o Need to separate between JDK..8 and JDK 9 cause    the error output looks different for JDK 9.

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5958LifecyclePhaseBinaryCompat.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5958LifecyclePhaseBinaryCompat.java
@@ -17,17 +17,37 @@ public class MavenITmng5958LifecyclePhaseBinaryCompat
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5958-lifecycle-phases/good" );
-        
+
         Verifier verifier = newVerifier( testDir.getAbsolutePath() );
         verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.verifyTextInLog( "CLASS_NAME=java.lang.String" );
         verifier.resetStreams();
     }
-    
-    public void testBad()
+
+    public void testBadTillJDK8()
         throws Exception
     {
+        requiresJavaVersion( "[,1.8]" );
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5958-lifecycle-phases/bad" );
+
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        try
+        {
+            verifier.executeGoal( "validate" );
+        }
+        catch ( VerificationException e )
+        {
+            verifier.verifyTextInLog( "[ERROR] Internal error: java.lang.ClassCastException: "
+                + "org.apache.maven.lifecycle.mapping.LifecyclePhase cannot be cast to java.lang.String -> [Help 1]" );
+        }
+        verifier.resetStreams();
+    }
+
+    public void testBadJDK9()
+        throws Exception
+    {
+        requiresJavaVersion( "[9.0,)" );
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5958-lifecycle-phases/bad" );
         
         Verifier verifier = newVerifier( testDir.getAbsolutePath() );
@@ -38,7 +58,7 @@ public class MavenITmng5958LifecyclePhaseBinaryCompat
         catch ( VerificationException e )
         {
             verifier.verifyTextInLog( "[ERROR] Internal error: java.lang.ClassCastException: "
-                + "org.apache.maven.lifecycle.mapping.LifecyclePhase cannot be cast to java.lang.String -> [Help 1]" );
+                + "org.apache.maven.lifecycle.mapping.LifecyclePhase cannot be cast to java.base/java.lang.String -> [Help 1]" );
         }
         verifier.resetStreams();
     }
